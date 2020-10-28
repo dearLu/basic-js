92874e1e19c2da1ec983fd0c32652dc1cccc5142
dearLu <lady.barabanovapolina@list.ru>
2020-10-28 22:56:29 +0400

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result='';
+  if(members){
+    for(let i=0;i<members.length;i++){
+      if (typeof members[i] == 'string' && members[i].trim()!=="") {
+          result+=(members[i].trim().split(''))[0];
+      }
+    }
+    sortedArr=result.split('');
+    /*for(let j=0; j<sortedArr.length;j++){
+
+    }*/
+   
+    return sortedArr.sort().join('').toUpperCase();
+  }
+  return false;
 };
