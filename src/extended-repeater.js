9eaab889c09a1abe8d7d057e551fb251e6b1da06
dearLu <lady.barabanovapolina@list.ru>
2020-10-31 22:45:59 +0400

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+let res='';
+let count;
+  if(String(options.repeatTimes)==='undefined'){
+    return str+String(options.addition);
+  }else{
+    count=options.repeatTimes;
+  }
+
+  for(let i=0;i<count;i++){
+    res+=String(str);
+    for(let j=0; j<options.additionRepeatTimes;j++){
+      
+        res+=String(options.addition);
+        if(j!=options.additionRepeatTimes-1){
+          if(String(options.additionSeparator).trim()==='undefined'){
+            res+='|';
+          }else{
+            res+=String(options.additionSeparator);
+          }
+        }      
+    }
+    if(i!=count-1 ){
+      if(String(options.separator).trim()==='undefined'){
+        res+='+';
+      }else{
+        res+=String(options.separator);
+      }
+    }
+    
+  }
+
+    return res;
+}
+
