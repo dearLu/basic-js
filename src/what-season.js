dec0351512e51514b223d47061d8f48e30859e58
dearLu <lady.barabanovapolina@list.ru>
2020-10-31 21:37:23 +0400

    feat: add hanoi tower

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date ) {
+let month;
+try{
+  if(date){
+   
+      if( date instanceof Date){        
+        month=new Date(date);
+        if(month!=0)
+          return findSeason(month.getMonth());
+        else 
+          throw new Error('THROWN') ; 
+      }else{
+          throw new Error('THROWN') ;
+      }
+      
+  }else{
+    return 'Unable to determine the time of year!';
+  }
+}catch(err){
+  throw new Error('THROWN') ;
+}
+
 };
+
+function findSeason(number){
+  if(number>=11 || number<2){
+    return 'winter';
+  }
+  if(number>=2 &&number<5){
+    return 'spring';
+  }
+  if(number>=5 && number<8){
+    return 'summer';
+  }
+  if(number>=8 && number<11){
+    return 'autumn';
+  }
+  return 0;
+}
