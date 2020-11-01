38d2744c378f5c1fa8255137ba4f4b5c04709912
dearLu <lady.barabanovapolina@list.ru>
2020-11-01 23:20:01 +0400

    feat: some add

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,59 +2,76 @@ const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
 
-
   constructor(notRerverse){
-    this.characters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    this.characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
     this.notRerverse=notRerverse;
     if(this.notRerverse === 'undefined') this.notRerverse =true;
+  }
+
+  getRepeatKey( s,  n){
+    var p = s;
+    while (p.Length < n)
+    {
+        p += p;
+    }
+
+  return p.substring(0, n);
   }
   
   encrypt(message,key) {
     let input = message.toUpperCase() ;
-    keyword = key.toUpperCase() ;
- 
+     
+    let keyword = this.getRepeatKey(key.toUpperCase(), input.Length);
+    let q = this.characters.length;
     let result = "";
- 
-    let keyword_index = 0;
- 
+
     for(let i=0; i<input.length;i++)
     {
-        let c = (Array.IndexOf(characters, input[i]) +
-            Array.IndexOf(characters, keyword[keyword_index])) % N;
- 
-        result += characters[c];
- 
-        keyword_index++;
- 
-        if ((keyword_index + 1) == keyword.Length)
-            keyword_index = 0;
+      let letterIndex = this.characters.indexOf(input[i],0);
+      let codeIndex = this.characters.indexOf(keyword[i],0);
+      if (letterIndex < 0)
+      {
+          result += input[i].toString();
+      }else{
+        result  += this.characters[(q + letterIndex + ( 1  * codeIndex)) % q].toString();
+      }
+      
     }
- 
+
+    if(String(this.notRerverse)==='false'){
+      return result.split("").reverse().join("");
+    }
     return result;
+    
   }    
   decrypt(message,key) {
     let input = message.toUpperCase() ;
-    keyword = key.toUpperCase() ;
- 
+     
+    let keyword = this.getRepeatKey(key.toUpperCase(), input.Length);
+    let q = this.characters.length;
     let result = "";
- 
-    let keyword_index = 0;
- 
+
     for(let i=0; i<input.length;i++)
     {
-        let p = (Array.IndexOf(characters, input[i]) + N -
-            Array.IndexOf(characters, keyword[keyword_index])) % N;
- 
-        result += characters[p];
- 
-        keyword_index++;
- 
-        if ((keyword_index + 1) == keyword.Length)
-            keyword_index = 0;
+      let letterIndex = this.characters.indexOf(input[i],0);
+      let codeIndex = this.characters.indexOf(keyword[i],0);
+      if (letterIndex < 0)
+      {
+          result += input[i].toString();
+      }else{
+        result  += this.characters[(q + letterIndex + ( -1  * codeIndex)) % q].toString();
+      }
     }
- 
+
+    if(String(this.notRerverse)==='false'){
+      return result.split("").reverse().join("");
+    }
     return result;
+    
   }
+  
+   
 }
 
 module.exports = VigenereCipheringMachine;
