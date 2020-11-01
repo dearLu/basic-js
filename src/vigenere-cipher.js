45a2c690431c2b4e9e70a1375f9e4b168b02a231
dearLu <lady.barabanovapolina@list.ru>
2020-11-01 20:31:28 +0400

    feat:add VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+
+  constructor(notRerverse){
+    this.characters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    this.notRerverse=notRerverse;
+    if(this.notRerverse === 'undefined') this.notRerverse =true;
+  }
+  
+  encrypt(message,key) {
+    let input = message.toUpperCase() ;
+    keyword = key.toUpperCase() ;
+ 
+    let result = "";
+ 
+    let keyword_index = 0;
+ 
+    for(let i=0; i<input.length;i++)
+    {
+        let c = (Array.IndexOf(characters, input[i]) +
+            Array.IndexOf(characters, keyword[keyword_index])) % N;
+ 
+        result += characters[c];
+ 
+        keyword_index++;
+ 
+        if ((keyword_index + 1) == keyword.Length)
+            keyword_index = 0;
+    }
+ 
+    return result;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message,key) {
+    let input = message.toUpperCase() ;
+    keyword = key.toUpperCase() ;
+ 
+    let result = "";
+ 
+    let keyword_index = 0;
+ 
+    for(let i=0; i<input.length;i++)
+    {
+        let p = (Array.IndexOf(characters, input[i]) + N -
+            Array.IndexOf(characters, keyword[keyword_index])) % N;
+ 
+        result += characters[p];
+ 
+        keyword_index++;
+ 
+        if ((keyword_index + 1) == keyword.Length)
+            keyword_index = 0;
+    }
+ 
+    return result;
   }
 }
 
