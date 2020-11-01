7a5362d69be3fb28c1f89666212c958a4f1d915b
dearLu <lady.barabanovapolina@list.ru>
2020-11-02 00:23:01 +0400

    feat: change transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -12,10 +12,10 @@ module.exports = function transform(arr) {
     if(arrExample[i] === '--double-next' && i+1!=arrExample.length){
       result.push(arrExample[i+1]);
     }
-    if(arrExample[i] ==='--double-prev' && i-1!=-1 &&  typeof arrExample[i-1] != 'string'){
+    if(arrExample[i] ==='--double-prev' && i-1!=-1 &&  typeof arrExample[i-1] != 'string' && arrExample[i-1] !='--discard-next'){
       result.push(arrExample[i-1]);
     }
-    if(arrExample[i] ==='--discard-prev' && i-1!=-1 ){     
+    if(arrExample[i] ==='--discard-prev' && i-1!=-1 && arrExample[i-1] !='--discard-next'){     
       result.splice(result.length-1, 1);
     }
     if(arrExample[i] ==='--discard-next' && i+1!=arrExample.length){
